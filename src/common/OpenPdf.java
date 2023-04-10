d2696fab28f60fb539eafdc4d50ea93f7527b3f1
Ronak2247232 <ronak.prasad@mca.christuniversity.in>
2023-04-10 21:51:28 +0530

    First Commit

---
+++ src/common/OpenPdf.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package common;
+import java.io.File;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author POULAMI
+ */
+public class OpenPdf {
+    public static void openById(String id){
+        try{
+            if((new File("D:\\"+id+".pdf")).exists()){
+                Process p = Runtime
+                        .getRuntime()
+                        .exec("rundll32 url.dll,FileProtocolHandler D:\\"+id+".pdf");
+                
+                
+            }
+            else
+                JOptionPane.showMessageDialog(null, "File does not Exist");
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+    
+}
